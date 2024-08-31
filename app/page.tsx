64defbf2b03363084618d08891d55029f93310d3
Mike👨‍💻 <binarybardcode@gmail.com>
2024-08-31 15:05:36 +0200

    Implement Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,32 @@ import Link from "next/link"
 
 const Home = () => {
   return (
-    <div>
-
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+            src="/assets/icons/logo-full.svg" 
+            alt="patient" 
+            width={1000}
+            height={1000} 
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image 
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   )
 }
