64b4b3b3d60ef8911972f736ef90081572abf699
Mike👨‍💻 <binarybardcode@gmail.com>
2024-09-01 23:20:57 +0200

    Implement Admin - Stat cards

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 "use server"; 
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+
 
 export const createAppointment = async (appointment : CreateAppointmentParams ) => {
     try {
@@ -32,3 +34,45 @@ export const getAppointment = async (appointmentId : string) => {
         console.log(error);
     }
  }  
+
+ export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc('$createdAt')]
+        );
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+              switch (appointment.status) {
+                case "scheduled":
+                  acc.scheduledCount++;
+                  break;
+                case "pending":
+                  acc.pendingCount++;
+                  break;
+                case "cancelled":
+                  acc.cancelledCount++;
+                  break;
+              }
+              return acc;
+            }, initialCounts); 
+
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        } 
+
+        return parseStringify(data); 
+        
+    } catch (error) {
+        console.log(error);
+    }
+ }   
