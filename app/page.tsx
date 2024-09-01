7ad1beb2e6b2eaf0883935b926e8ca31ebd1b491
Mike👨‍💻 <binarybardcode@gmail.com>
2024-09-01 21:55:17 +0200

    Implement Admin PassCode verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm"
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = () => {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP VERIFICATION | PASSKEY MODAL */} 
+      { isAdmin && <PasskeyModal />} 
       
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
